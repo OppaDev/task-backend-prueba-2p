d05c837ce3cb08076a2559f4e33f9e6217704d27
Leonardo <jlobando2@espe.edu.ec>
2024-07-23 10:14:30 -0500

    actualization

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TaskViewSet, RegisterView, LoginView
 
+
+
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 
